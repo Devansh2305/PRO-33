df9f0717583c6553d11bb802d47d082c2305f1cc
Devansh <67364840+Devansh2305@users.noreply.github.com>
2020-09-01 18:17:47 +0530

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,23 @@
+class Particle{
+    constructor(x,y,r){
+        var options={
+            restitution:0.4,
+            
+        }
+        
+        this.body = Bodies.circle(x,y,r,options);
+        this.color=color(random(0,255),random(0,255),random(0,255));
+        this.r=r;
+        World.add(world,this.body);
+    }
+    display(){
+       fill(this.color)
+        push();
+        ellipse(this.body.position.x, this.body.position.y,this.r*2,this.r*2);
+        pop();
+        noFill();
+      }
+}
+
+
+ 
